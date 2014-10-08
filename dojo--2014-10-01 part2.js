a608c4c5132be9ebedcf002c62f43f593dd6247d
Keith Kreider <kreider1997@att.net>
2014-10-08 21:32:48 +0000

    initial commit @kreider1997 and @fab9

---
+++ dojo--2014-10-01 part2.js
@@ -0,0 +1,22 @@
+/**
+ * Check Writing
+ *
+ * GIVEN: a number like 1234.56
+ * WHEN: called with `toEnglish()`
+ * THEN: expect 'one thousand two hundred thirty four and 56/100s'
+ */
+/** === PRODUCTION CODE === **/
+
+/**
+ * Function Definition: toEnglish()
+ *
+ * @name toEnglish Identifier
+ *
+ * @param Number value
+ * @return String
+ *
+ */
+ var assert = require("assert");
+ 
+
+de
