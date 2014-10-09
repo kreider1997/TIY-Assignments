db2e859e02b1dd76510f60c040661260cc11235e
Keith Kreider <kreider1997@att.net>
2014-10-09 20:05:12 +0000

    dojo update

--- dojo--2014-10-01 part2.js
+++ dojo--2014-10-01 part2.js
@@ -19,4 +19,17 @@
  var assert = require("assert");
  
 
-de
+ function toEnglish(value){
+     var ones = { "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10};
+    return ones[value];
+     
+ }
+
+describe('toEnglish()', function() {
+    it ('should have function named toEnglish()', function(){
+
+        assert.equal(toEnglish(0), "zero");
+        assert.equal(toEnglish(1), "one");
+    });
+    
+});
