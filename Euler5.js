914fb7b72de4e18ce5abc48ece66eead2c8a8de7
Keith Kreider <kreider1997@att.net>
2014-10-10 19:25:17 +0000

    defining the functions for Euler5

--- Euler5.js
+++ Euler5.js
@@ -2,8 +2,32 @@
 //
 //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-var assert = require('assert');
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var should = require('chai').should();
 
+var array19 = [];
+//array19.fill(1++[start = 1 [end = 1000000]]);
+
+function smallestNum (){
+  
+}
+
+
+
+
+//   TESTING
+
+describe('smallestNum()', function(){
+    it('should be a type of function named smallestNum()', function(){
+        assert.typeOf(smallestNum,'function');
+    });
+});
+
+
+
+
+/*
 var eulerFive = function(){
   for (var i=0; i<=10000000; i++){
     for (var num = 1; num<= 20; num++){
@@ -17,7 +41,8 @@ var eulerFive = function(){
 
 
 describe ('eulerFive'(), function (){
-  it('should return number divisable by all integers 1-20', funciton(){
+  it('should return number divisable by all integers 1-20', function(){
     assert.equal(eulerFive(), ?????);
   });
 });
+*/
