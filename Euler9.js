165d4b2d5869760cf91e4541ddc04a0f9717b881
Keith Kreider <kreider1997@att.net>
2014-10-10 10:57:40 -0400

    adding files

---
+++ Euler9.js
@@ -0,0 +1,19 @@
+//A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+//
+//a2 + b2 = c2
+//For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+//There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+//Find the product abc.
+
+
+
+var pythagorean = function (a, b, c,){
+  for (a=0; a<b; a++){
+    for (b=0; b<c; b++){
+      if ((a*a + b*b == c*c) && (a+b+c=1000))
+        return c
+    }
+
+  }
+}
--- MultiArray-10-8.js
+++ MultiArray-10-8.js
@@ -1,4 +1,4 @@
-// Multidimensional array example
+// Multidimensional array example.  Website:  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#Example:_Creating_a_two-dimensional_array
 var board = [
   ['R','N','B','Q','K','B','N','R'],
   ['P','P','P','P','P','P','P','P'],
@@ -9,3 +9,9 @@ var board = [
   ['p','p','p','p','p','p','p','p'],
   ['r','n','b','q','k','b','n','r'],
 ];
+console.log(board.join('\n') + '\n\n');
+
+// move kings pawn forward 2
+board [4][4] = board [6][4];
+board [6][4] = ' ';
+console.log(board.join('\n'));
