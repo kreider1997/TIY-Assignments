3caa8f3ea837d811f9ee2acd990939526ed7c957
Keith Kreider <kreider1997@att.net>
2014-10-14 16:28:12 -0400

    updating master with local terminal

--- dojo--2014-10-03.js
+++ dojo--2014-10-03.js
@@ -135,7 +135,7 @@
 //}
 //  START  HERE
 
-var assert = require('assert');
+var assert = require('chai').assert;
 
 //below sets up the board and defines the neighbors of a specific cell.  Using rw for row, cl for column.
 var board =  [
@@ -144,6 +144,85 @@ var board =  [
         [ false, false, false ],
     ];
 
+//using code from loganarnet and phillycc
+
+var game = {
+  board: undefined,
+  newBoard: function (){
+    return this.board = [
+      [ false, true, false ],
+      [ false, true, false ],
+      [ false, true, false ].
+    ];
+  },
+  rules: function(cell, neighbors){
+    var liveNeighbors = neighbors.filter(funciton(value){
+      return value;
+    });
+    liveNeighbors = liveNeighbors.length;
+    //apply rules
+    if (cell && liveNeighbors === 2)
+      return true;
+    if (liveNeighbors === 3)
+      return true;
+    return false;
+  },
+  neighborsOf: function(x, y){
+    var diffs = [ -1, 0, +1 ],
+    neighbors = [ ];
+
+    diffs.forEach(function(dX){
+      diff.forEach(function(dY){
+        if(this.board.lengh===0)
+          return;
+        // skip this cell
+        if ( dX === 0 && dY === 0 )
+          return;
+        // skip if row index does not exist
+        if (((x + dX) < 0) || ((x + dX) >2))
+          return;
+        //skip if col index does not exist
+        if (((y + dY) < 0) || ((y + dY)>2))
+          return;
+        neighbors.push(this.board[x + dX] [y+ dY]);
+      });
+    });
+    return neighbors;
+  },
+  tick: function(){
+    var after = this.board;
+    this.board.forEach(function(row, x){
+      row.forEach(function(cell, y){
+        after [x][y]=game.rules(cell, game.neighborsOf(x,y));
+      });
+    });
+    return after;
+  },
+  display: function(){
+    var spacer = '+---+---+---+\n';
+
+    return spacer +
+      //apply 'renderRow' to each 'row' in 'board'....
+      this.board.map(function renderRow(row){
+        return '| ' +
+        //apply `renderCell` to each `cell` in `row`...
+        row.map(function renderCell(cell){
+          // return 'X' if `cell` is TRUTHY otherwise return ' '
+          return cell && 'X' || ' ';
+        }).join(' | ')  //Place ' | ' between each `cell`...
+        + ' |\n';
+      }).join(spacer) //Place `spacer` between each `row`...
+      + spacer;
+  }// END display
+}// END game
+console.log (game.display(game.tick(game.newBoard())));
+
+
+// TEST AREA
+
+
+
+/*  Old code
 var neighbors;
 
 function neighborsOf (board, rw, cl){
@@ -201,9 +280,13 @@ function conway (cell, neighbors) {
       }
       return newCell
 }
-//TEST AREA
+
+*/
 
 
+
+
+/*  old  tests
 function test(actual, expected, success){
     if (success === undefined) success = 'pass!';
 
@@ -221,3 +304,4 @@ var testb5 = neighborOf (board, 1, 2)
 var testb6 = neighborOf (board, 2, 0)
 var testb7 = neighborOf (board, 2, 1)
 var testb8 = neighborOf (board, 2, 2)
+*/
